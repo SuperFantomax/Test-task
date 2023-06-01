dc75f7dbc3b7b3c779839e0bf1318d9b02feeb72
SuperFantomax <135238247+SuperFantomax@users.noreply.github.com>
2023-06-01 19:52:57 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,27 @@
 import os
-import shutil
+import hashlib
+import pickle
+import logging
+import argparse
 
 source_path = "C:\\test\\Source"
 replica_path = "C:\\test\\Replica"
 
-def copy_job():
-    shutil.copytree(source_path, replica_path, dirs_exist_ok=True)
+list_original = os.listdir(source_path)
+list_replica = os.listdir(replica_path)
 
+def hash_calculation(list):
+    binary = pickle.dumps(list)
+    hash_list = hashlib.md5(binary)
+    return hash_list.hexdigest()
 
-copy_job()
+hashnumber_source = hash_calculation(list_original)
+hashnumber_replica = hash_calculation(list_replica)
+
+def sync_of_data():
+    if hashnumber_source == hashnumber_replica:
+        print('There is no changes.')
+    else:
+        print('Upsy')
+            
+sync_of_data()
