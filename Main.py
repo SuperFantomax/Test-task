61b23da2120ccf217bc60fc56528ad25b5c86d02
SuperFantomax <135238247+SuperFantomax@users.noreply.github.com>
2023-06-02 11:27:55 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -18,10 +18,27 @@ def hash_calculation(list):
 hashnumber_source = hash_calculation(list_original)
 hashnumber_replica = hash_calculation(list_replica)
 
-def sync_of_data():
-    if hashnumber_source == hashnumber_replica:
-        print('There is no changes.')
-    else:
-        print('Upsy')
+
+if hashnumber_source != hashnumber_replica:
+    print("Synchronization needed.")
+
+    for item in list_replica:
+        item_path = os.path.join(replica_path, item)
+        if os.path.isfile(item_path):
+            os.remove(item_path)
+        elif os.path.isdir(item_path):
+            os.rmdir(item_path)
+
+    for item in list_original:
+        src_path = os.path.join(source_path, item)
+        rpl_path = os.path.join(replica_path, item)
+        if os.path.isfile(src_path):
+            with open(src_path, 'rb') as src_file, open(rpl_path, 'wb') as rpl_file:
+                content = src_file.read()
+                rpl_file.write(content)
+        elif os.path.isdir(src_path):
+            os.mkdir(rpl_path)
             
-sync_of_data()
+    print("Synchronization completed.")
+else:
+    print("No synchronization needed. Source and replica directories are already synchronized.")
