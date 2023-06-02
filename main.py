10d38eff94b633c5f1fb0ed7a57436e181f78d8b
SuperFantomax <135238247+SuperFantomax@users.noreply.github.com>
2023-06-02 15:11:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,16 +13,14 @@ def calculate_file_hash(file_path):
         file_hash = hash_object.hexdigest()
         return file_hash
     
-source_hash = {}    
-for root, dirs, files in os.walk(source_path):
-    for file in files:
-        file_path_source = os.path.join(root, file)
-        file_hash_source = calculate_file_hash(file_path_source)
-        source_hash[file_path_source] = file_hash_source
+def file_hashes(directory):
+    file_hashes = {}
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            file_path = os.path.join(root, file)
+            file_hash = calculate_file_hash(file_path)
+            file_hashes[file_path] = file_hash
+    return file_hashes
 
-replica_hash = {}
-for root, dirs, files in os.walk(replica_path):
-    for file in files:
-        file_path_replica = os.path.join(root, file)
-        file_hash_replica = calculate_file_hash(file_path_replica)
-        replica_hash[file_path_replica] = file_hash_replica
+print(file_hashes(source_path))
+print(file_hashes(replica_path))
