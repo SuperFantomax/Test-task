479bdb6745ca13b98fbb67eaabe8a653633c1989
SuperFantomax <135238247+SuperFantomax@users.noreply.github.com>
2023-06-05 10:02:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,22 @@ if __name__ == "__main__":
     import hashlib
     import logging
     import argparse
+    import time
 
-    source_path = "C:\\test\\Source"
-    replica_path = "C:\\test\\Replica"
+    parser = argparse.ArgumentParser(description='Synchronization of 2 folders')  
+    parser.add_argument('source_path', type=str, help='enter address of your source folder')
+    parser.add_argument('replica_path', type=str, help='enter address of your replica folder')
+    parser.add_argument('log_path', type=str, help='enter address of your log file')
+    parser.add_argument('time_interval', type=float, help='enter interval for syncronization')
+    args = parser.parse_args()
+
+    source_path = args.source_path
+    replica_path = args.replica_path
+    log_path = args.log_path
+    time_interval = args.time_interval
+
+    logging.basicConfig(filename=log_path, level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
     def file_set(directory):
         file_set = set()
@@ -23,41 +36,86 @@ if __name__ == "__main__":
 
     source_set = file_set(source_path)
     replica_set = file_set(replica_path)
+           
+    def calculate_md5(path):
+        md5 = hashlib.md5()
 
-    print(source_set)
-    print(replica_set)
+        if os.path.isfile(path):
+            with open(path, 'rb') as file:
+                while True:
+                    data = file.read()
+                    if not data:
+                        break
+                    md5.update(data)
 
-   
+        return md5.hexdigest()
 
-    if source_set != replica_set:
-        print("Synchronization needed.")
+    def calculate_subtree_md5(directory):
+        md5 = hashlib.md5()
 
-        for item in replica_set:
-            item_path = os.path.join(replica_path, item)
-            if os.path.isfile(item_path):
-                os.remove(item_path)
-            elif os.path.isdir(item_path):
-                os.rmdir(item_path)
+        for root, dirs, files in os.walk(directory):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_md5 = calculate_md5(file_path)
+                md5.update(file_md5.encode('utf-8'))
 
-        for item in source_set:
-            src_path = os.path.join(source_path, item)
-            rpl_path = os.path.join(replica_path, item)
-            if os.path.isfile(src_path):
-                src_file = open(src_path, 'rb')
-                rpl_file = open(rpl_path, 'wb')
-                content = src_file.read()
-                rpl_file.write(content)
-            elif os.path.isdir(src_path):
-                os.mkdir(rpl_path)
+        return md5.hexdigest()
+
+    source_hash = calculate_subtree_md5(source_path)
+    replica_hash = calculate_subtree_md5(replica_path)
+                
+    def sync():
+        delete_set = replica_set.difference(source_set)
+        create_set = source_set.difference(replica_set)
+        samedata_set = replica_set.intersection(source_set)
+
+        if len(delete_set) or len(create_set) != 0:
+            print("Synchronization needed.")
+
+            for item in delete_set:
+                item_path = os.path.join(replica_path, item)
+                if os.path.isfile(item_path):
+                    os.remove(item_path)
+                    logging.info(f'File {item} from {replica_path} was deleted.')
+                elif os.path.isdir(item_path):
+                    os.rmdir(item_path)
+                    logging.info(f'Directory {item} from {replica_path} was deleted.')
+                print(f'Next files was deleted: {item}')
+
+            for item in create_set:
+                src_path = os.path.join(source_path, item)
+                rpl_path = os.path.join(replica_path, item)
+                if os.path.isfile(src_path):
+                    src_file = open(src_path, 'rb')
+                    rpl_file = open(rpl_path, 'wb')
+                    content = src_file.read()
+                    rpl_file.write(content)
+                    logging.info(f'File {item} from {source_path} was added to the {replica_path}.')
+                    print(f'Next files has been created: {item}')
+                elif os.path.isdir(src_path):
+                    os.mkdir(rpl_path)
+                    logging.info(f'Directory {item} from {source_path} was added to the {replica_path}.')
+                    print(f'Next directories has been created: {item}')
             
-        print("Synchronization completed.")
-    else:
-        print("No synchronization needed. Source and replica are already synchronized.")
-
-    def calculate_file_hash(file_path):
-    with open(file_path, 'rb') as file:
-        content = file.read()
-        hash_object = hashlib.md5(content)
-        file_hash = hash_object.hexdigest()
-        return file_hash
+        elif len(samedata_set) > 0 and source_hash != replica_hash:
+            for item in samedata_set:
+                if source_hash != replica_hash:
+                    rpl_path = os.path.join(replica_path, item)
+                    src_path = os.path.join(source_path, item)
+                    if os.path.isfile(rpl_path):
+                        os.remove(rpl_path)
+                        src_file = open(src_path, 'rb')
+                        rpl_file = open(rpl_path, 'wb')
+                        content = src_file.read()
+                        rpl_file.write(content)
+                        logging.info(f'Files {item} from {replica_path} has been replaced with files from {source_path}')
+                        print(f"Next files was replaced: {item}")
+                                
+            print("Synchronization completed.")
+                  
+        else:
+            print("No synchronization needed. Source and replica are already synchronized.")
     
+    while True:
+        sync()
+        time.sleep(time_interval)
