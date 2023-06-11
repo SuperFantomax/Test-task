d86fe21ef50d88071bca3c3d148b57642e9acb1a
SuperFantomax <135238247+SuperFantomax@users.noreply.github.com>
2023-06-11 16:15:07 +0300

    if source not a directory or does not exists

--- msync/main.py
+++ msync/main.py
@@ -26,6 +26,10 @@ def calculate_md5(path):
     return md5.hexdigest()
 
 def sync(source_path, replica_path):
+    if not os.path.exists(source_path):
+        raise FileNotFoundError(f"Source folder '{source_path}' does not exist.")
+    if not os.path.isdir(source_path):
+        raise NotADirectoryError(f'Source path {source_path} is not a directory.')
     if not os.path.exists(replica_path):
         os.makedirs(replica_path)
         logging.info(f'Replica folder {replica_path} created.')
